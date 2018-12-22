0825205fb00577201fa7f2119062a41c96519478
Ammar Ameerdeen <ammar@keeneye.us>
2018-12-22 11:08:20 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RecentUpdatesComponent } from './recent-updates/recent-updates.component';
+import { ExpeiriencesComponent } from './expeiriences/expeiriences.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent,
+    RecentUpdatesComponent,
+    ExpeiriencesComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/expeiriences/expeiriences.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expeiriences',
+  templateUrl: './expeiriences.component.html',
+  styleUrls: ['./expeiriences.component.css']
+})
+export class ExpeiriencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
